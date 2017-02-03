07727a20b8875adb7d4808694539897acba0ed3d
Jobarah <barahona.josue@unitec.edu>
2017-02-03 15:21:50 -0600

    temp running

---
+++ WaterBottleSchedule/src/config/employees.js
@@ -0,0 +1,18 @@
+module.exports = {
+	"employee1" : {"username":"Raul Argueta", "email": "rargueta@sanservices.hn"},
+	"employee2" : {"username":"Josué Rodríguez", "email": "jrodríguez@sanservices.hn"},
+	"employee3" : {"username":"Josue Enamorado", "email": "jenamorado@sanservices.hn"},
+	"employee4" : {"username":"Luis Booth", "email": "lbooth@sanservices.hn"},
+	"employee5" : {"username":"Douglas Barahona", "email": "dbarahona@sanservices.hn"},
+	"employee6" : {"username":"Lester Barahona", "email": "lbarahona@sanservices.hn"},
+	"employee7" : {"username":"Fernando Mejia", "email": "fmejia@sanservices.hn"},
+	"employee8" : {"username":"Edwin Edwards", "email": "ecedwards@sanservices.hn"},
+	"employee9" : {"username":"Fernando Lontero", "email": "flontero@sanservices.hn"},
+	"employee10" : {"username":"Jose Turcios", "email": "jturcios@sanservices.hn"},
+	"employee11" : {"username":"Edwin Herrera", "email": "eherrera@sanservices.hn"},
+	"employee12" : {"username":"David Nuñez", "email": "dnunez@sanservices.hn"},
+	"employee13" : {"username":"Jose Muñoz", "email": "jmunoz@sanservices.hn"},
+	"employee14" : {"username":"Eric Edwards", "email": "eedwards@sanservices.hn"},
+	"employee15" : {"username":"Luis Espinoza", "email": "lespinoza@sanservices.hn"},
+	"employee16" : {"username":"Luis Matute", "email": "lmatute@sanservices.hn"}
+};
